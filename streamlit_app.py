9e094222822b4f79f6588395988243a54a2230b9
mileidy1211 <mileidyblanco1211@gmail.com>
2026-03-29 10:36:46 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ st.markdown("""
    
     /* Sombra en widgets del sidebar al pasar el mouse */
     [data-testid="stSidebar"] div[data-testid="stWidgetLabel"] + div:hover {
-        box-shadow: 0 10px 15px rgba(0,0,0,0.3);
+        box-shadow: 0 15px 15px rgba(0,0,0,0.5);
         transition: 0.3s;
     }
 
